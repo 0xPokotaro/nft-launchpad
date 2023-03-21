b2e5165778165d5804830ec0498af8f9040bdc9f
kmasuda1986 <kentarou_masuda@hacklab-tech.io>
2023-03-21 11:01:16 +0900

    fix: Add loading

--- frontend/src/pages/launchpad.tsx
+++ frontend/src/pages/launchpad.tsx
@@ -7,6 +7,7 @@ const Launchpad: React.FC = () => {
   const [currentSupply, setCurrentSupply] = useState(0);
   const [price, setPrice] = useState(0);
   const [mintDeadline, setMintDeadline] = useState<Date | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     // Fetch data for NFT launchpad from an API or contract
@@ -16,36 +17,82 @@ const Launchpad: React.FC = () => {
     setMintDeadline(new Date('2023-04-30T00:00:00Z'));
   }, []);
 
+  const [timeLeft, setTimeLeft] = useState<{ days: number; hours: number; minutes: number; seconds: number }>({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 4000);
+  
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    if (!mintDeadline) {
+      return;
+    }
+
+    // タイマーを設定
+    const timerId = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = mintDeadline.getTime() - now;
+
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      setTimeLeft({ days, hours, minutes, seconds });
+    }, 1000);
+
+    // タイマーを停止するために返却する関数を設定
+    return () => clearInterval(timerId);
+  }, [mintDeadline]);
+
   const handleMint = async () => {
     // Implement logic to mint NFT
     console.log('Minting NFT...');
   };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.content}>
-        <h2 className={styles.title}>NFT Launchpad</h2>
-        <div className={styles.stats}>
-          <div className={styles.stat}>
-            <span className={styles.label}>Total Supply:</span> {totalSupply}
-          </div>
-          <div className={styles.stat}>
-            <span className={styles.label}>Current Supply:</span> {currentSupply}
-          </div>
-          <div className={styles.stat}>
-            <span className={styles.label}>Price:</span> {price} ETH
-          </div>
-          <div className={styles.stat}>
-            <span className={styles.label}>Mint Deadline:</span>{' '}
-            {mintDeadline?.toLocaleDateString()}
-          </div>
+    <>
+      {isLoading ? (
+        <div className={isLoading ? styles.loading : styles.loaded}>
+          <div className={styles.spinner}></div>
+          <div className={styles.text}>{isLoading && 'NFT will bring a sweet and beautiful fragrance to your life. Experience that fragrance.'}</div>
         </div>
-        <button className={styles.button} onClick={handleMint}>
-          Mint NFT
-        </button>
-      </div>
-      <ParticleBackground />
-    </div>
+      ) : (
+        <div className={styles.container}>
+          <div className={styles.content}>
+            <h2 className={styles.title}>NFT Launchpad</h2>
+            <div className={styles.stats}>
+              <p className={styles.stat}>
+                <span className={styles.label}>Total Supply:</span> {totalSupply}
+              </p>
+              <p className={styles.stat}>
+                <span className={styles.label}>Current Supply:</span> {currentSupply}
+              </p>
+              <p className={styles.stat}>
+                <span className={styles.label}>Price:</span> {price} ETH
+              </p>
+              <p className={styles.stat}>
+                <span className={styles.label}>Mint Deadline:</span>{' '}
+                {mintDeadline?.toLocaleDateString()}
+              </p>
+            </div>
+            <button className={styles.button} onClick={handleMint}>
+              Mint NFT
+            </button>
+          </div>
+          <ParticleBackground />
+        </div>
+      )}
+    </>
   );
 };
 
